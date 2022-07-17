64e07a5a57f63e2114006cd7d3399e62a3295547
Qa <you@example.com>
2022-07-17 13:17:24 +0800

    o

--- mariadb/condition.go
+++ mariadb/condition.go
@@ -6,7 +6,6 @@ import (
 	"strings"
 
 	"github.com/jigadhirasu/qtool/types"
-	"gorm.io/datatypes"
 	"gorm.io/gorm"
 )
 
@@ -27,13 +26,12 @@ func column(field ...string) string {
 }
 
 type C struct {
-	Table     string   // 資料表明
-	UUID      string   // 唯一編號
-	Field     []string // 有索引的屬性
-	Attr      []string // 沒索引的屬性
-	Index     KV       // 有索引的條件
-	Condition KV       // 沒索引條件
-	Query     *Query   // 搜尋頁數及排序
+	Table string   // 資料表明
+	UUID  string   // 唯一編號
+	Field []string // 有索引的屬性
+	Attr  []string // 沒索引的屬性
+	Index KV       // 有索引的條件
+	// Condition KV       // 沒索引條件
 }
 
 func (c C) Select() string {
@@ -61,11 +59,11 @@ func (c C) Where(tx *gorm.DB) *gorm.DB {
 		tx = tx.Where("UUID = ?", c.UUID)
 	}
 	// 僅索引可以搜尋
-	for k, v := range c.Index {
-		tx = tx.Where(k+" = ?", v)
-	}
-	for k, v := range c.Condition {
-		tx = tx.Where(datatypes.JSONQuery("Doc").Equals(v, k))
+	// for k, v := range c.Index {
+	// 	tx = tx.Where(k+" = ?", v)
+	// }
+	if c.Index != nil {
+		tx = c.Index.Where(tx)
 	}
 	return tx
 }
@@ -89,10 +87,10 @@ func (c C) Value(tx *gorm.DB) types.Bytes {
 
 func (c C) Values(tx *gorm.DB) types.Bytes {
 	tx = c.Where(tx)
-	if c.Query != nil {
-		tx = c.Query.Limit(tx)
-		tx = c.Query.OrderBy(tx)
-	}
+	// if c.Query != nil {
+	// 	tx = c.Query.Limit(tx)
+	// 	tx = c.Query.OrderBy(tx)
+	// }
 	bb := [][]byte{}
 	tx.Limit(1).Pluck(fmt.Sprintf(`JSON_ARRAYAGG(%s)`, c.Select())+" as Doc", &bb)
 	if len(bb) > 0 {
@@ -104,17 +102,101 @@ func (c C) Values(tx *gorm.DB) types.Bytes {
 type KV map[string]interface{}
 
 func (kv KV) Where(tx *gorm.DB) *gorm.DB {
-	for k, v := range kv {
-		switch str := v.(type) {
-		case string:
-			if ok, _ := regexp.MatchString("^LT([w-])$", strings.ToUpper(str)); ok {
 
-			}
-		case []string:
-			tx = tx.Where(k+" IN ?", v)
+	isDep, _ := regexp.Compile("^\\$\\.")
+
+	for k, v := range kv {
+		if ok := isDep.MatchString(k); ok {
+			k = fmt.Sprintf("JSON_VALUE(Doc,'%s')", k)
 		}
 
+		switch t := v.(type) {
+		case int:
+			tx = tx.Where(k+" = ?", t)
+		case []string:
+			tx = tx.Where(k+" IN ?", t)
+		case string:
+			if p := kv.EQ(t); p != nil {
+				tx = tx.Where(k+" = ?", p)
+			}
+			if p := kv.LIKE(t); p != nil {
+				tx = tx.Where(k+" LIKE ?", p)
+			}
+			if p := kv.LT(t); p != nil {
+				tx = tx.Where(k+" < ?", p)
+			}
+			if p := kv.LTE(t); p != nil {
+				tx = tx.Where(k+" <= ?", p)
+			}
+			if p := kv.GT(t); p != nil {
+				tx = tx.Where(k+" > ?", p)
+			}
+			if p := kv.GTE(t); p != nil {
+				tx = tx.Where(k+" >= ?", p)
+			}
+			if p := kv.IN(t); p != nil {
+				tx = tx.Where(k+" IN ?", p)
+			}
+			if p := kv.NOT(t); p != nil {
+				tx = tx.Where(k+" NOT ?", p)
+			}
+		}
 	}
 
 	return tx
+}
+
+func (KV) EQ(str string) interface{} {
+	if ok, _ := regexp.MatchString(":", str); !ok {
+		return str
+	}
+	if ok, _ := regexp.MatchString("^EQ:", strings.ToUpper(str)); ok {
+		return str[3:]
+	}
+	return nil
+}
+func (KV) LIKE(str string) interface{} {
+	if ok, _ := regexp.MatchString("^LIKE:", strings.ToUpper(str)); ok {
+		str = strings.ReplaceAll(str, "$", "%")
+		return str[5:]
+	}
+	return nil
+}
+
+func (KV) LT(str string) interface{} {
+	if ok, _ := regexp.MatchString("^LT:", strings.ToUpper(str)); ok {
+		return str[3:]
+	}
+	return nil
+}
+func (KV) LTE(str string) interface{} {
+	if ok, _ := regexp.MatchString("^LTE:", strings.ToUpper(str)); ok {
+		return str[4:]
+	}
+	return nil
+}
+func (KV) GT(str string) interface{} {
+	if ok, _ := regexp.MatchString("^GT:", strings.ToUpper(str)); ok {
+		return str[3:]
+	}
+	return nil
+}
+func (KV) GTE(str string) interface{} {
+	if ok, _ := regexp.MatchString("^GTE:", strings.ToUpper(str)); ok {
+		return str[4:]
+	}
+	return nil
+}
+func (KV) IN(str string) interface{} {
+	if ok, _ := regexp.MatchString("^IN:", strings.ToUpper(str)); ok {
+		str = str[3:]
+		return strings.Split(str, ",")
+	}
+	return nil
+}
+func (KV) NOT(str string) interface{} {
+	if ok, _ := regexp.MatchString("^NOT:", strings.ToUpper(str)); ok {
+		return str[4:]
+	}
+	return nil
 }
