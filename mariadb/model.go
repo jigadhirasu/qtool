8913999af23e1b0b9ba437cf98f7b2a112967f82
Qa <you@example.com>
2022-07-17 13:55:31 +0800

    model-

--- mariadb/model.go
+++ mariadb/model.go
@@ -9,86 +9,108 @@ import (
 	"gorm.io/gorm"
 )
 
-type Model interface {
-	TableName() string
-	UU(uuid ...string) string
-	Owner() string
+func TableName(m interface{}) string {
+	v := reflect.Indirect(reflect.ValueOf(m))
+	f := v.MethodByName("TableName")
+	if f.IsValid() {
+		return f.Interface().(func() string)()
+	}
+	panic("UUID invalid")
 }
 
-type Instance interface {
-	Model
-	Type() Model
+func UUID(m interface{}) string {
+	v := reflect.Indirect(reflect.ValueOf(m))
+	f := v.FieldByName("UUID")
+	if f.IsValid() {
+		return f.String()
+	}
+	panic("UUID invalid")
+}
+
+func Owner(m interface{}) string {
+	v := reflect.Indirect(reflect.ValueOf(m))
+	f := v.MethodByName("TableName")
+	if f.IsValid() {
+		return f.Interface().(func() string)()
+	}
+	return ""
 }
 
 // 有紀錄
-func Create(tags types.Tags, m Model) func(db *gorm.DB) types.Bytes {
+func Create(tags types.Tags, m interface{}) func(db *gorm.DB) types.Bytes {
 	r := &Record{
 		OpID:     tags.String("User"),
-		OwnerID:  m.Owner(),
 		Method:   "create",
-		Target:   m.TableName(),
-		TargetID: m.UU(),
+		Target:   TableName(m),
+		TargetID: UUID(m),
 		OpBefore: []byte(`{}`),
 		OpAfter:  types.JSON(m),
 	}
+	if o := Owner(m); o != "" {
+		r.OwnerID = o
+	}
 	pk := &Pack{Doc: types.JSON(m)}
 	return func(db *gorm.DB) types.Bytes {
-		tx := db.Table(m.TableName()).Create(pk)
+		tx := db.Table(TableName(m)).Create(pk)
 		if tx.Error != nil {
 			return types.JSON(Result{Error: types.DBErr(tx.Error)})
 		}
 		db.Table(r.TableName()).Create(&Pack{Doc: types.JSON(r)})
 		return types.JSON(Result{
-			UUID:  m.UU(),
+			UUID:  UUID(m),
 			Error: types.DBErr(tx.Error),
 		})
 	}
 }
 
 // 沒紀錄
-func Createx(tags types.Tags, m Model) func(db *gorm.DB) types.Bytes {
+func Createx(tags types.Tags, m interface{}) func(db *gorm.DB) types.Bytes {
 	pk := &Pack{Doc: types.JSON(m)}
 	return func(db *gorm.DB) types.Bytes {
-		tx := db.Table(m.TableName()).Create(pk)
+		tx := db.Table(TableName(m)).Create(pk)
 		if tx.Error != nil {
 			return types.JSON(Result{Error: types.DBErr(tx.Error)})
 		}
 		return types.JSON(Result{
-			UUID:  m.UU(),
+			UUID:  UUID(m),
 			Error: types.DBErr(tx.Error),
 		})
 	}
 }
 
-func Delete(tags types.Tags, m Model) func(db *gorm.DB) types.Bytes {
+func Delete(tags types.Tags, m interface{}) func(db *gorm.DB) types.Bytes {
 	r := &Record{
 		OpID:     tags.String("User"),
-		OwnerID:  m.Owner(),
 		Method:   "delete",
-		Target:   m.TableName(),
-		TargetID: m.UU(),
+		Target:   TableName(m),
+		TargetID: UUID(m),
 		OpAfter:  []byte(`{}`),
 	}
+	if o := Owner(m); o != "" {
+		r.OwnerID = o
+	}
 	return func(db *gorm.DB) types.Bytes {
-		r.OpBefore = C{Table: m.TableName(), UUID: m.UU()}.Value(db)
-		tx := db.Table(m.TableName()).Where("UUID = ?", m.UU()).Delete(m)
+		r.OpBefore = C{Table: TableName(m), UUID: UUID(m)}.Value(db)
+		tx := db.Table(TableName(m)).Where("UUID = ?", UUID(m)).Delete(m)
 		db.Table(r.TableName()).Create(&Pack{Doc: types.JSON(r)})
 		return types.JSON(Result{
-			UUID:  m.UU(),
+			UUID:  UUID(m),
 			Error: types.DBErr(tx.Error),
 		})
 	}
 }
 
 // return Result
-func Update(tags types.Tags, m Model, fields ...string) func(db *gorm.DB) types.Bytes {
+func Update(tags types.Tags, m interface{}, fields ...string) func(db *gorm.DB) types.Bytes {
 	r := &Record{
 		OpID:     tags.String("User"),
-		OwnerID:  m.Owner(),
 		Method:   "update",
 		Field:    strings.Join(fields, ","),
-		Target:   m.TableName(),
-		TargetID: m.UU(),
+		Target:   TableName(m),
+		TargetID: UUID(m),
+	}
+	if o := Owner(m); o != "" {
+		r.OwnerID = o
 	}
 
 	oldPtr := reflect.New(reflect.Indirect(reflect.ValueOf(m)).Type())
@@ -111,13 +133,13 @@ func Update(tags types.Tags, m Model, fields ...string) func(db *gorm.DB) types.
 
 	return func(db *gorm.DB) types.Bytes {
 		// *** 檢查異動欄位 ***
-		v := C{Table: m.TableName(), UUID: m.UU()}.Value(db)
+		v := C{Table: TableName(m), UUID: UUID(m)}.Value(db)
 		if len(v) < 1 {
 			return types.JSON(Result{Error: types.NewErr(520, "data not found")})
 		}
 		v.Decode(o)
-		SQL := fmt.Sprintf("UPDATE %s SET Doc = JSON_MERGE_PATCH(Doc, ?) WHERE UUID = ?", m.TableName())
-		tx := db.Exec(SQL, types.JSON(n), m.UU())
+		SQL := fmt.Sprintf("UPDATE %s SET Doc = JSON_MERGE_PATCH(Doc, ?) WHERE UUID = ?", TableName(m))
+		tx := db.Exec(SQL, types.JSON(n), UUID(m))
 		if err := diff(o, n); err != nil {
 			return types.JSON(Result{Error: types.DBErr(err)})
 		}
@@ -138,14 +160,16 @@ func Update(tags types.Tags, m Model, fields ...string) func(db *gorm.DB) types.
 }
 
 // return Result
-func Push(tags types.Tags, m Model, field string) func(db *gorm.DB) types.Bytes {
+func Push(tags types.Tags, m interface{}, field string) func(db *gorm.DB) types.Bytes {
 	r := &Record{
 		OpID:     tags.String("User"),
-		OwnerID:  m.Owner(),
 		Method:   "push",
 		Field:    field,
-		Target:   m.TableName(),
-		TargetID: m.UU(),
+		Target:   TableName(m),
+		TargetID: UUID(m),
+	}
+	if o := Owner(m); o != "" {
+		r.OwnerID = o
 	}
 
 	vm := reflect.ValueOf(m)
@@ -158,9 +182,9 @@ func Push(tags types.Tags, m Model, field string) func(db *gorm.DB) types.Bytes 
 	newPtr := reflect.New(reflect.Indirect(reflect.ValueOf(m)).Type())
 
 	return func(db *gorm.DB) types.Bytes {
-		SQL := fmt.Sprintf(`UPDATE %s SET doc = JSON_SET(doc, '$.%s', COALESCE(JSON_VALUE(doc, '$.%s'), 0) + ?) WHERE UUID = ?`, m.TableName(), field, field)
-		tx := db.Exec(SQL, f.Interface(), m.UU())
-		v := C{Table: m.TableName(), UUID: m.UU(), Field: []string{"UUID"}, Attr: []string{field}}.Value(db)
+		SQL := fmt.Sprintf(`UPDATE %s SET doc = JSON_SET(doc, '$.%s', COALESCE(JSON_VALUE(doc, '$.%s'), 0) + ?) WHERE UUID = ?`, TableName(m), field, field)
+		tx := db.Exec(SQL, f.Interface(), UUID(m))
+		v := C{Table: TableName(m), UUID: UUID(m), Field: []string{"UUID"}, Attr: []string{field}}.Value(db)
 		if len(v) < 1 {
 			return types.JSON(Result{Error: types.NewErr(520, "data not found")})
 		}
@@ -191,7 +215,7 @@ func Push(tags types.Tags, m Model, field string) func(db *gorm.DB) types.Bytes 
 // // return Result
 // func Find(m IQuery) func(db *gorm.DB) types.Bytes {
 // 	return func(db *gorm.DB) types.Bytes {
-// 		tx := m.Where(db).Table(m.TableName())
+// 		tx := m.Where(db).Table(TableName(m))
 
 // 		var count int64
 // 		tx.Count(&count)
@@ -213,7 +237,7 @@ func Push(tags types.Tags, m Model, field string) func(db *gorm.DB) types.Bytes 
 // // return Result
 // func Findx(m IQuery) func(db *gorm.DB) types.Bytes {
 // 	return func(db *gorm.DB) types.Bytes {
-// 		tx := m.Where(db).Table(m.TableName())
+// 		tx := m.Where(db).Table(TableName(m))
 
 // 		var count int64
 // 		tx.Count(&count)
